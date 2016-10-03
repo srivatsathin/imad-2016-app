31f2ffc8faad6d5e5df12bc2dbec6c7e6ebae887
srivatsathin <srivatsathindran@gmail.com>
2016-10-03 19:29:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ app.get('/article-three', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
+app.get('/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
