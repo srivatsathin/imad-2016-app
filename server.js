04c4b87d9d5149ae222f70da76b6f0d3f96658f9
srivatsathin <srivatsathindran@gmail.com>
2016-10-03 15:09:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get(' /article-one', function (req, res) {
+  res.send('Article one request will be served here boss');
+});
+
+
+app.get(' /article-two', function (req, res) {
+  res.send('Article two request will be served here boss');
+});
+
+app.get(' /article-three', function (req, res) {
+  res.send('Article three request will be served here boss');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
