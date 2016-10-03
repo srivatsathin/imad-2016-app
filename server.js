aae4da3cb9ac0d32a1afca98c953ff6b0c2f8954
srivatsathin <srivatsathindran@gmail.com>
2016-10-03 15:51:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.get('/article-one', function (req, res) {
 
 
 app.get('/article-two', function (req, res) {
-  res.send('Article two request will be served here boss');
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
 });
 
 app.get('/article-three', function (req, res) {
-  res.send('Article three request will be served here boss');
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
