416da3a63f657133429e5dc58ab4ebca4f97e875
srivatsathin <srivatsathindran@gmail.com>
2016-10-08 12:42:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ app.get('/:articlename', function (req, res) {
 
 var counter= 0;
 app.get('/counter', function (req, res) {
-counter= counter +1;
+counter = counter + 1;
 res.send(counter.toString());
 });
 
