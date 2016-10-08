2615675ba26688559e802b928270bd22e7f0d736
srivatsathin <srivatsathindran@gmail.com>
2016-10-08 13:13:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,24 @@
 var button= document.getElementById("counter");
-var counter= 0;
-button.onclick = function(){
+
+button.onclick = function()
+{
     
     
+    var request = new XMLHttpRequest();
     
+    request.onreadystatechange = function()
+    {
+        if (request.readystate == XMLHttpRequest.DONE)
+        {
+           if (request.response == 200)
+           {
+               var counter = request.responsetext;
+              var span= document.getElementById("count");
+              span.innerHTML = counter.toString();  
+           }
+        }
+    };
+    request.open('GET', 'http://http://srivatsathin.imad.hasura-app.io//counter', true);
+    request.send(null);
     
-    
-    
-    counter = counter +1 ;
-    var span= document.getElementById("count");
-    span.innerHTML = counter.toString();
 };
