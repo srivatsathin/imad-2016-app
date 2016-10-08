37ac62b4c396e6ba7f973c9bf59459e933921c46
srivatsathin <srivatsathindran@gmail.com>
2016-10-08 12:58:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,13 @@
-console.log('Loaded!');
-var element= document.getElementById("main-text");
-element.innerHTML=('newtext');
-
-var img = document.getElementById("madi");
-var marginLeft = 0;
-function moveRight(){
-    marginLeft= marginLeft +5 ;
-    img.style.marginLeft = marginLeft + 'px' ;
-}
-img.onclick=function(){
-    var interval= setInterval(moveRight,10); 
+var buton= document.getElementById("counter");
+var counter= 0;
+button.onclick = function(){
+    
+    
+    
+    
+    
+    
+    counter = counter +1 ;
+    var span= document.getElementById("count");
+    span.innerHTML = counter.toString();
 };
-
