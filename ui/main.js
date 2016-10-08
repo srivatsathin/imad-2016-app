7046711854c735b86a735c4590694c45926f99d5
srivatsathin <srivatsathindran@gmail.com>
2016-10-08 08:47:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element= document.getElementById("main-text");
 element.innerHTML=('newtext');
+
+var img = document.getElementById("madi");
+img.onclick=function(){
+    img.style.marginleft = '100 px' ; 
+};
+
