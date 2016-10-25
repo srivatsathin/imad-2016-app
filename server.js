84b2f2aaf3aa4015814a7f2a878c92ebc007a1d5
srivatsathin <srivatsathindran@gmail.com>
2016-10-25 10:34:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -169,9 +169,24 @@ return htmltemplate;
 }
      
 
-app.get('/:articlename', function (req, res) {
-    var articlename=req.params.articlename;
-  res.send(Createtemplate(articles[articlename]));
+app.get('/articles/:articlename', function (req, res) {
+    
+    pool.query("SELECT * FROM article WHERE title=" + req,params.articlename, function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        } 
+        else{
+            if(result.rows.length ===0){
+                res.status(404).send('Article not found');
+            } else{
+                var articleData= result.rows[0];
+                 res.send(Createtemplate(articleData));
+            }
+        }
+    }
+    
+);
+    
 });
 
 
