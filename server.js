b7f51fe44c31d476a854e06ec181e1304bfcd858
srivatsathin <srivatsathindran@gmail.com>
2016-10-25 13:17:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -176,7 +176,7 @@ app.get('/articles/:articlename', function (req, res) {
             res.status(500).send(err.toString());
         } 
         else{
-            if(result.rows.length ===0){
+            if(result.rows.length === 0){
                 res.status(404).send('Article not found');
             } else{
                 var articleData= result.rows[0];
