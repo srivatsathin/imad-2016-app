384f01ebb6c9d347090bacb9609a2609606cfd87
srivatsathin <srivatsathindran@gmail.com>
2016-10-25 13:22:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ return htmltemplate;
 
 app.get('/articles/:articlename', function (req, res) {
     
-    pool.query("SELECT * FROM article WHERE title= ' " + req.params.articlename + " ' ", function(err,result){
+    pool.query("SELECT * FROM article WHERE title= " + req.params.articlename, function(err,result){
         if(err){
             res.status(500).send(err.toString());
         } 
